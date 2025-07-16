c2e87f3a31de44fa5daebe01b25f85f2b0aa009a
rodri <rodrihercar24@gmail.com>
2025-07-15 22:45:17 -0600

    Creacion de clase alumnos

--- alumnos.py
+++ alumnos.py
@@ -1,2 +1,7 @@
 # rama de alumnos en el programa
 
+class Alumnos:
+    def __init__(self, name:str, password:str, course:list):
+        self.name = name
+        self.password = password
+        self.course = course
