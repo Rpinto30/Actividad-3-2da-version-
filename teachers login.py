656182ffd87ca756ef7744d34e61afc11e233abe
Aileen127 <fleuraileen394@gmail.com>
2025-07-16 20:56:54 -0600

    Modificación del menú y ciclo while para uso del menú, iniciando con la primera opción

--- teachers login.py
+++ teachers login.py
@@ -3,7 +3,18 @@
 def menu_login():
     print("\n Bienvenido al menú")
     print("1. Ingresar nuevo usuario")
-    print("2. Ingresar usuario existente")
-    print("3. Modificar usuario existente")
-    print("4. Salir del menú")
+    print("2. Modificar nombre de usuario existente")
+    print("3. Modificar código de usuario existente")
+    print("3. Salir del menú")
+
+while True:
+    menu_login()
+
+    option = input("Ingresa la opción que deseas ejecutar (1-3")
+
+    if option == "1":
+        teacher = input("Ingresa el nombre del docente: ")
+        code = input("Ingresa el código de identificación del docente: ")
+        u.add_user(teacher, code)
+        print("El usuario ha sido agregado correctamente.")
 
