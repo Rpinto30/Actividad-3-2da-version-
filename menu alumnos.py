4a76a31434038206103dee46885884789491aaf9
rodri <rodrihercar24@gmail.com>
2025-07-16 18:30:24 -0600

    creacion de la estructura de condicionales del menu de alumnos

--- menu alumnos.py
+++ menu alumnos.py
@@ -1,9 +1,20 @@
 # Creacion menu de alumnos
 while True:
     print("----Menu Alumnos----\n"
-          "1. Consultar notas\n"
+          "\n1. Consultar notas\n"
           "2. Asignacion de cursos\n"
           "3. Desasignacion de cursos\n"
           "4. Salir")
-    option = input("Ingresa una opcion: ")
+    option = input("\nIngresa una opcion: ")
+    if option == "1":
+        pass
+    elif option == "2":
+        pass
+    elif option == "3":
+        pass
+    elif option == "4":
+        print("\nRegresando al menu principal")
+        break
+    else:
+        print("Ingresa una opcion valida")
 
