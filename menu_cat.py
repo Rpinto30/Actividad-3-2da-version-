09a65a9548aea79a2b60854e5316347bbaad5f82
Rpinto30 <rodrigopinto3007@gmail.com>
2025-07-16 22:17:34 -0600

    Menpú de catedraticos

--- menu_cat.py
+++ menu_cat.py
@@ -1,2 +1,14 @@
 def menu_cat(cat):
-    pass
+    while True:
+        print("Opciones:" +
+              "\n  1) Agregar nota\n  2) Mostrar alumnos\n  3) Mostrar promedios\n  4) Salir")
+        op = int(input("Elige una de las siguientes opciones: "))
+
+        if op == 1:
+            pass
+        elif op == 2:
+            pass
+        elif op == 3:
+            pass
+        else:
+            break
