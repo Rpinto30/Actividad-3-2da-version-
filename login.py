45954c13b1788bba693e824e0fee04be514f3658
Aileen127 <fleuraileen394@gmail.com>
2025-07-16 21:35:02 -0600

    funcion que modifica al usuario ya existente, eliminandolo del registro del diccionario, así agreganddo los nuevos datos.

--- login.py
+++ login.py
@@ -9,3 +9,11 @@ class Login:
             print(f"El docente {teacher}, con el código {code} ha sido ingresado correctamente")
         else:
             print(f"El docente {teacher} ya ha sido agregado previamente.")
+
+    def edit_user(self, teacher, new_teacher):
+        if teacher in self.login:
+            code = self.login[teacher] #Ayuda a que no se genere error al ingresar los datos en el diccionario
+            del self.login[teacher]
+            self.login[new_teacher] = code # Se ingresan los datos en el diccionario en el orden correcto
+            print(self.login)
+
