37dd81820685114a861d200865dd0f5b5450cd84
rodri <rodrihercar24@gmail.com>
2025-07-16 18:26:45 -0600

    primera parte del menu de alumnos

---
+++ menu alumnos.py
@@ -0,0 +1,9 @@
+# Creacion menu de alumnos
+while True:
+    print("----Menu Alumnos----\n"
+          "1. Consultar notas\n"
+          "2. Asignacion de cursos\n"
+          "3. Desasignacion de cursos\n"
+          "4. Salir")
+    option = input("Ingresa una opcion: ")
+
