58119d7d115e7fff9dc1d3371a7c789754109cf7
Rpinto30 <rodrigopinto3007@gmail.com>
2025-07-17 00:45:36 -0600

    Arreglos en todos lados x'd II

--- menu_alumnos.py
+++ menu_alumnos.py
@@ -2,7 +2,7 @@
 from cursos import Course
 from alumnos import Alumnos
 
-def alumnos_menu(alum):
+def alumnos_menu(alum, courses):
     while True:
         print("----Menu Alumnos----\n"
               "\n1. Consultar notas\n"
@@ -12,29 +12,23 @@ def alumnos_menu(alum):
         option = input("\nIngresa una opcion: ")
         if option == "1":
             print("---Cursos---nota---docente")
-            if len(alum.cursos) > 0:
-                for i in alum.course: #ciclo for para mostrar lo contenido en la lista de curso
-                    print(f"{i.name}, {i.score}, {i.teacher}")
-            else:
-                print("No hay cursos asignadoos")
-
+            for i in alum.course:
+                print(f"{i.name}, {i.score}, {i.teacher}")
         elif option == "2":
-            cours = input("\nIngrese a que curso se quiere asignar: ").lower()
-            cat = input("escoge al catedratico: ")
-            alum.course.append(Course(cours, cat, 0))
+            for num,i in enumerate(courses): print(f"  {num+1})Nombre:{i.name}  Catedratico:{i.teacher.name}  Creditos:{i.credits}")
 
             cours = int(input("\nIngrese a que curso se quiere asignar: "))
-            if cours <=  int(num)+1: alum.course.append([cour[num-1], 0])
+            if cours <=  int(num)+1: alum.course.append([courses[num-1], 0])
             else: print("Lo siento, entrada invalida")
 
             print("Curso asignado!")
         elif option == "3":
             print("---Nombre---Carnet---")
             print(alum.name, alum.card)
-            print("--Cursos asignados--Notas")
-            for i in alum.course: print(f"  {i[0].name}  {i[1]} ")
         elif option == "4":
             print("\nRegresando al menu principal")
             break
         else:
             print("Ingresa una opcion valida")
+
+
