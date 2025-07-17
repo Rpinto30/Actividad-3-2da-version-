a6541b564f84919eeebc4e3e8ea50fd6a988b73f
Aileen127 <fleuraileen394@gmail.com>
2025-07-16 20:20:17 -0600

    Menú login teachers

---
+++ teachers login.py
@@ -0,0 +1,9 @@
+# login teachers
+
+def menu_login():
+    print("\n Bienvenido al menú")
+    print("1. Ingresar nuevo usuario")
+    print("2. Ingresar usuario existente")
+    print("3. Modificar usuario existente")
+    print("4. Salir del menú")
+
