849abee9b350257e904b9f9e0e37ed31402113b1
Rpinto30 <rodrigopinto3007@gmail.com>
2025-07-16 22:13:44 -0600

    Agregar funcion teahcer al main

---
+++ profesor.py
@@ -0,0 +1,6 @@
+class Teacher:
+    def __init__(self, name: str, password: str, course: str, alums: list):
+        self.name = name
+        self.password = password
+        self.course = course
+        self.alums = alums
