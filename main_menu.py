5bd2a133925729b4c9e2c7c02141c8b2b56c7b93
Rpinto30 <rodrigopinto3007@gmail.com>
2025-07-16 18:30:45 -0600

    Bucle de menu principal

--- main_menu.py
+++ main_menu.py
@@ -1,4 +1,21 @@
-print("Bienvenidos")
+'''
+MENU PRINCIPAL
+'''
 
+print("-"*10+"Bienvenidos"+"-"*10)
 while True:
-    pass
+    print("Opciones:" +
+          "\n  1) Catedratico\n  2) Alumnos\n  3) Crear nuevo usuario\n  4) Salir")
+    op = int(input("Elige una de las siguientes opciones: "))
+
+    if op == 1:
+        pass
+    elif op == 2:
+        pass
+    elif op == 3:
+        pass
+    else:
+        break
+
+print("Hasta pronto!")
+
