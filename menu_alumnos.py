1b3b0b2f2c302204c5c5da3fd90090a543f6da67
rodri <rodrihercar24@gmail.com>
2025-07-16 21:27:21 -0600

    cambio de opcio a datos personales

--- menu_alumnos.py
+++ menu_alumnos.py
@@ -1,24 +1,27 @@
 # Creacion menu de alumnos
 from cursos import Course
+from alumnos import Alumnos
 
 def alumnos_menu(alum):
     while True:
         print("----Menu Alumnos----\n"
               "\n1. Consultar notas\n"
               "2. Asignacion de cursos\n"
-              "3. Desasignacion de cursos\n"
+              "3. Datos personales\n"
               "4. Salir")
         option = input("\nIngresa una opcion: ")
         if option == "1":
+            print("---Cursos---nota---docente")
             for i in alum.course:
-                print(i.name, i.score)
+                print(f"{i.name}, {i.score}, {i.teacher}")
         elif option == "2":
             cours = input("\nIngrese a que curso se quiere asignar: ").lower()
             cat = input("escoge al catedratico: ")
             alum.course.append(Course(cours, cat, 0))
 
         elif option == "3":
-            pass
+            print("---Nombre---Carnet---")
+            print(alum.name, alum.card)
         elif option == "4":
             print("\nRegresando al menu principal")
             break
